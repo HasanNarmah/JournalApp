8325653ee8e620147afc9f428f2002a8820d6c5a
Hasan Narmah <narmah.hasan@gmail.com>
2023-12-27 19:06:02 +0000

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Hasan Narmah on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
